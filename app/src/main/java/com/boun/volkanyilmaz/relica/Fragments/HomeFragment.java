c6d51f817ee2410fc583db2f945b05f5bdf19339
Volkan Yılmaz <volkan.yilmazboun@gmail.com>
2018-03-13 16:00:23 +0200

    Added sendRequestRefresh

--- app/src/main/java/com/boun/volkanyilmaz/relica/Fragments/HomeFragment.java
+++ app/src/main/java/com/boun/volkanyilmaz/relica/Fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -40,6 +41,7 @@ public class HomeFragment extends Fragment {
     private List<MemoryModel> modelList;
     private RecyclerView recyclerView;
     private TextView textView;
+    private SwipeRefreshLayout refreshLayout;
 
 
     private String url = "http://10.0.2.2/Relica/getMemories.php";
@@ -54,7 +56,6 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
@@ -69,7 +70,13 @@ public class HomeFragment extends Fragment {
 
         sendRequest();
 
-
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                modelList.clear();
+                sendRequestRefresh();
+            }
+        });
 
 
         Log.d("Volley test", ".............................................");
@@ -147,13 +154,86 @@ public class HomeFragment extends Fragment {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
 
 
-
-
     }
-    private void setAdapter(){
 
-        RecyclerView.Adapter mAdapter = new MyAdapter(modelList,context);
+    private void setAdapter() {
+
+        RecyclerView.Adapter mAdapter = new MyAdapter(modelList, context);
         recyclerView.setAdapter(mAdapter);
 
     }
+
+
+    private void sendRequestRefresh() {
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                refreshLayout.setRefreshing(false);
+                Log.d("Json data Memories: ", response);
+
+                String status = null, message = null;
+                JSONArray memories = null;
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    status = jsonObject.getString("status");
+                    message = jsonObject.getString("message");
+                    memories = jsonObject.getJSONArray("memories");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                //Success
+                if (status.equals("200")) {
+
+                    for (int i = 0; i < memories.length(); i++) {
+                        JSONObject memory;
+                        MemoryModel model = new MemoryModel();
+                        try {
+                            memory = memories.getJSONObject(i);
+                            model.setFullname(memory.getString("fullname"));
+                            model.setUsername(memory.getString("username"));
+                            model.setProfilePath(memory.getString("avatar"));
+                            model.setImagePath(memory.getString("path"));
+                            model.setMemoryText(memory.getString("text"));
+                            model.setDate(memory.getString("date"));
+                        } catch (JSONException e) {
+                            Log.e("json parse error", e.getLocalizedMessage());
+                        }
+
+                        modelList.add(model);
+
+                    }
+
+                } else {
+                    //request failed
+                    Snackbar.make(recyclerView, message, Snackbar.LENGTH_LONG).show();
+                }
+                Log.d("Volley operations test", "request operations completed.........................");
+                if (modelList == null) {
+                    textView.setText("Memory object cannot be null...");
+                    setAdapter();
+                } else {
+                    setAdapter();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                refreshLayout.setRefreshing(false);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", id);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(request);
+    }
+
 }
